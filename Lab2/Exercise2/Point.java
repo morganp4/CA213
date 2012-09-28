9b57e2ae78ed6e030d73bb6cf3dc8f1215bc2db3
Ian Duffy <ian@ianduffy.ie>
2012-09-28 17:42:18 +0100

    Add Lab2 Exercise2

---
+++ Lab2/Exercise2/Point.java
@@ -0,0 +1,13 @@
+class Point implements Printable {
+    private int x;
+    private int y;
+    
+    Point(int x0, int y0) {
+        x = x0;
+        y = y0;
+    }
+    
+    public void put() {
+        System.out.println("("+x+","+y+")");
+    }
+}
