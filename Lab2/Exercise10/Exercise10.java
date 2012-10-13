4959f74d4131d87e78c6650d5eb63ea91aba516e
Ian Duffy <ian@ianduffy.ie>
2012-10-14 00:32:53 +0100

    Add Exercise10

---
+++ Lab2/Exercise10/Exercise10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Exercise10 {
+    static <T extends Comparable<T>> void sort(T[] s) {
+        for(int i=0; i<s.length; i++) {
+            for(int j = i+1; j<s.length; j++) {
+                if(s[i].compareTo(s[j])>0) {
+                    T tmp;
+                    tmp = s[i];
+                    s[i] = s[j];
+                    s[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Pete","Jill","May","Anne","Tim"};
+        sort(names);
+        for(String s : names )  {
+            System.out.println(s);
+        }
+    }
+}
