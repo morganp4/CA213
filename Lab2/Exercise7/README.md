12b956302bbe7849f77c312b63894a0763462a62
Ian Duffy <ian@ianduffy.ie>
2012-10-28 00:33:39 +0100

    Restructure and add Exams

--- Lab2/Exercise7/README.md
+++
@@ -1,7 +0,0 @@
-Tic Tac Toe
-===========
-
-Basic version of Tic Tac Toe wrote in Java.
-See notes.txt for information on the winning combos.
-
-![tic tac toe](https://github.com/imduffy15/CA213/raw/master/Lab2/Exercise7/screenshot.png "Screenshot of Tic Tac Toe")
--- Lab2/Exercise8/Exercise8.java
+++
@@ -1,8 +0,0 @@
-class Exercise8 {
-    public static void main(String[] args) {
-        Pair<String,Integer> phoneNumber = new Pair<String,Integer>("Bill's number", 1234);
-        System.out.println(phoneNumber);
-        Pair<Double,Double> point = new Pair<Double,Double>(3.14, 2.32);
-        System.out.println(point);
-    }
-}
