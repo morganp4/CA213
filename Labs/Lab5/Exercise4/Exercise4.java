a3f4f4106ab2ba20340980891233ad309d050853
Ian Duffy <ian@ianduffy.ie>
2012-11-19 00:13:52 +0000

    Fix zero bug

--- Labs/Lab5/Exercise4/Exercise4.java
+++ Labs/Lab5/Exercise4/Exercise4.java
@@ -1,14 +1,18 @@
 class Exercise4 {
-    static String writeNum(int n) {
-        if(n < 1000) {
-            return "" + n;
+    static void writeNum(int n) {
+        String w = ""+n;
+        if(w.length() < 4) {
+            System.out.print(w);
+        } else {
+            writeNum(Integer.parseInt(w.substring(0,w.length()-3)));
+            System.out.print("," + w.substring(w.length()-3));
         }
-        return "" + (writeNum(n/1000) + "," + (n%1000));
     }
 
     public static void main(String[] args) {
         System.out.println("Type in a number:");
         int input = Console.readInt();
-        System.out.println(writeNum(input));
+        writeNum(input);
+        System.out.println();
     }
 }
