8482b9c93c77173bac7bd732e7e8067c43808377
Ian Duffy <ian@ianduffy.ie>
2012-11-28 21:46:42 +0000

    Add Lab6 ex1

---
+++ Labs/Lab6/Exercise1/Exercise1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Exercise1 {
+    public static void main(String[] args) {
+        MyArrayList<String> ws = new MyArrayList<String>();
+        ws.addFirst("cat"); ws.addFirst("dog");
+        ws.addFirst("cow"); ws.addFirst("dog");
+        ws.addFirst("pig");
+        System.out.println(ws.indexOf("dog"));
+        ws.remove(ws.indexOf("dog"));
+        System.out.println(ws.indexOf("dog"));
+        System.out.println(ws.indexOf("hen"));
+    }
+}
