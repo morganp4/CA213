d418fa95cf58df3410bb7142e70c634711685ac4
Ian Duffy <ian@ianduffy.ie>
2012-12-10 11:03:21 +0000

    Add test file for exercise3 (still need to fix code)

---
+++ Labs/Lab8/Exercise3/Exercise3.java
@@ -0,0 +1,16 @@
+class Exercise3 {
+    public static void main(String[] args) {
+        MyTreeSet<Integer> items = new MyTreeSet<Integer>();
+        items.add(3);
+        items.add(5);
+        items.add(1);
+        items.add(2);
+        items.add(4);
+        items.add(7);
+        items.add(6);
+        System.out.println(items.breadthFirst());
+        System.out.println(items.pollFirst());
+        System.out.println(items.breadthFirst());
+        System.out.println(items.pollFirst());
+    }
+}
