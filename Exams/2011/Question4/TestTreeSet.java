5fcab50a643243d0b7f5217ecdc5787a5e10a9a0
Ian Duffy <ian@ianduffy.ie>
2012-12-25 18:16:05 +0000

    Add 2011 Question 4

---
+++ Exams/2011/Question4/TestTreeSet.java
@@ -0,0 +1,13 @@
+class TestTreeSet {
+    public static void main(String[] args) {
+        TreeSet<Integer> items = new TreeSet<Integer>();
+        items.add(3);
+        items.add(5);
+        items.add(2);
+        items.add(1);
+        items.add(4);
+        items.add(7);
+        items.add(6);
+        System.out.println(items.max());
+    }
+}
---
+++ Exams/2011/Question4/TreeSet.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class TreeSet<T extends Comparable<T>> {
+    private static class Node<T> {
+        private T item;
+        private Node<T> left, right;
+
+        Node(T item0, Node<T> left0, Node<T> right0) {
+            item = item0;
+            left = left0;
+            right = right0;
+        }
+    }
+
+    private Node<T> root = null;
+    private int numItems = 0;
+
+    public int size() {
+        return numItems = 0;
+    }
+
+    public boolean add(T t) {
+        int numItems0 = numItems;
+        root = add(root,t);
+        return (numItems0<numItems);
+    }
+    private Node<T> add(Node<T> p, T t) {
+        if (p==null) {
+            numItems++;
+            return new Node<T>(t,null,null);
+        } else if ((p.item).compareTo(t)>0) {
+            p.left = add(p.left,t);
+            return p;
+        } else if (t.compareTo(p.item)>0) {
+            p.right = add(p.right,t);
+            return p;
+        } else {
+            return p;
+        }
+    }
+
+    T max() {
+        if(root == null) return null;
+        Node<T> p = root;
+        while(p.right!=null) {
+            p = p.right;
+        }
+        return p.item;
+    }
+}
